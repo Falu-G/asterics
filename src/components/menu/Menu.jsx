dd6ef1b017fe9b63ea14833c3ba0a33cffb23354
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-08-16 09:18:07 +0100

    refactoring

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -13,40 +13,40 @@ function Menus() {
             <div className="menuHouse">
                 <div className="firstMenu">
 
-                    <Link className="menuItem" to = "/maindashboard">
+                    <Link className="menuItem" to="/maindashboard">
                         <MenuIcon />
                         <span>Main</span>
-                    </Link> 
+                    </Link>
 
-                   
-                        <Link className="menuItem" to = "/dashboard">
+
+                    <Link className="menuItem" to="/dashboard">
                         <MenuIcon />
                         <span>Dashboard</span>
-                        </Link>
-                        
-                    
+                    </Link>
 
-                    <Link className="menuItem" to = "/customer">
+
+
+                    <Link className="menuItem" to="/customer">
                         <MenuIcon />
                         <span>Customer</span>
                     </Link>
 
-                    <Link className="menuItem" to ="/sendsms">
+                    <Link className="menuItem" to="/sendsms">
                         <MenuIcon />
                         <span>Send SMS</span>
                     </Link>
 
-                    <Link className="menuItem" to = "/sendemai">
+                    <Link className="menuItem" to="/sendemai">
                         <MenuIcon />
                         <span>Send Email</span>
                     </Link>
 
-                    <Link className="menuItem" to = "/schedule">
+                    <Link className="menuItem" to="/schedule">
                         <MenuIcon />
                         <span>Schedules</span>
                     </Link>
 
-                    <Link className="menuItem" to = "">
+                    <Link className="menuItem" to="">
                         <MenuIcon />
                         <span>Templates</span>
                     </Link>
