997a34ddeca5adf981609a70372d5530946e7b99
Bamidele Omonayin <bamideleomonayin@gmail.com>
2021-08-24 20:43:21 +0000

    design sign up

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import TextField from '@material-ui/core/TextField';
 
 import "./signUp.css"
@@ -40,85 +39,102 @@ function SignUp() {
     }
 
     return (
-        <div className="formContainer">
-            <form Validate className="signUpForm">
-
-                <h3>SignUp</h3>
-                <TextField
-                    id="companyName"
-                    label="Company name"
-                    variant="outlined"
-                    onChange={(e) => setCompanyName(e.target.value)}
-                    value={companyname}
-
-                />
-
-
-                <hr size={3} />
-
-
-                <div className="firstfield">
-                    <TextField
-                        id="firstname"
-                        label="First Name"
-                        variant="outlined"
-                        className="firstfieldin"
-                        onChange={(e) => setFirstName(e.target.value)}
-                        value={firstName}
-                    />
-
-                    <TextField
-                        style={{ marginLeft: '10px' }}
-                        id="lastname"
-                        label="Last Name"
-                        variant="outlined"
-                        className="firstfieldin"
-                        onChange={(e) => setLastName(e.target.value)}
-                        value={lastName}
-                    />
-
+        <div className="formContainer" style={{
+            backgroundImage: `url(/images/signupbg.png)`
+            , backgroundRepeat: 'no-repeat', backgroundSize: 'contain',
+            backgroundPosition: 'center left'
+        }}>
+            <div className="firstSignUpContainer">
+                <div  className="firstSignUp">
+                <span>SignUp</span>
+                <h4>Create your new account</h4>
+                <p>and manage your business with a single Single sign</p>
                 </div>
+                
 
+            </div>
+            <div className="secondSignUp">
+                <form Validate className="signUpForm">
 
-
-                <div className="secondfield">
+                    <h3>Create an account</h3>
                     <TextField
-                        id="standard-error-helper-text"
-                        label="Email"
+                        id="companyName"
+                        label="Company name"
                         variant="outlined"
-                        className="secondfieldin"
-                        onChange={(e) => setEmail(e.target.value)}
-                        value={email}
+                        onChange={(e) => setCompanyName(e.target.value)}
+                        value={companyname}
+
                     />
 
+
+                    <hr size={3} />
+
+
+                    <div className="firstfield">
+                        <TextField
+                            id="firstname"
+                            label="First Name"
+                            variant="outlined"
+                            className="firstfieldin"
+                            onChange={(e) => setFirstName(e.target.value)}
+                            value={firstName}
+                        />
+
+                        <TextField
+                            style={{ marginLeft: '10px' }}
+                            id="lastname"
+                            label="Last Name"
+                            variant="outlined"
+                            className="firstfieldin"
+                            onChange={(e) => setLastName(e.target.value)}
+                            value={lastName}
+                        />
+
+                    </div>
+
+
+
+                    <div className="secondfield">
+                        <TextField
+                            id="standard-error-helper-text"
+                            label="Email"
+                            variant="outlined"
+                            className="secondfieldin"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                        />
+
+                        <TextField
+                            style={{ marginLeft: '10px' }}
+                            id="standard-error-helper-text"
+                            label="Username"
+                            variant="outlined"
+                            className="secondfieldin"
+                        />
+
+                    </div>
                     <TextField
-                        style={{ marginLeft: '10px' }}
                         id="standard-error-helper-text"
                         label="Username"
                         variant="outlined"
-                        className="secondfieldin"
                     />
 
-                </div>
-                <TextField
-                    id="standard-error-helper-text"
-                    label="Username"
-                    variant="outlined"
-                />
 
+                    <TextField
+                        id="standard-error-helper-text"
+                        label="Username"
+                        variant="outlined"
+                        className="formfield"
+                    />
 
-                <TextField
-                    id="standard-error-helper-text"
-                    label="Username"
-                    variant="outlined"
-                    className="formfield"
-                />
-
-                <div> <button className="Regbtn" onClick={signUp}>Register</button></div>
+                    <div> <button className="Regbtn" onClick={signUp}>Register</button></div>
 
 
 
-            </form>
+                </form>
+
+            </div>
+
 
         </div>
     )
