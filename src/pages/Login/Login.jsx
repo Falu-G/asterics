600b507bc1bc46af1dff5264d049890ea9a13375
Bamidele Omonayin <bamideleomonayin@gmail.com>
2021-08-25 20:50:42 +0000

    laoding the loading bar

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,40 +1,47 @@
-import React, {useState} from 'react'
-import { Link ,useHistory} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import './login.css'
+import { Progress } from 'react-sweet-progress';
 function Login() {
     let history = useHistory();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("")
+    const [showIcon, setShowIcon] = useState(false)
+
+    const [errorMessage, setErrorMessage] = useState("")
+
+    const [loginError, setLoginError] = useState(false)
 
 
-    const login = async (e)=>{
+    const login = async (e) => {
 
         e.preventDefault();
         console.warn(email, password)
-        let item = {email:email,password:password}
-        let result = await fetch("https://asteric.herokuapp.com/users/authenticate",{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json",
-                "Accept":"application/json"
+        let item = { email: email, password: password }
+        let result = await fetch("https://asteric.herokuapp.com/users/authenticate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
             },
-            body:JSON.stringify(item)
+            body: JSON.stringify(item)
         })
 
         result = await result.json();
-       
 
-        if(result.status === "success"){
+
+        if (result.status === "success") {
             console.log(result)
-            localStorage.setItem("user-info",result)
+            localStorage.setItem("user-info", result)
             history.push("/maindashboard")
-        } else{
-            console.warn(result.message)
+        } else {
+            setErrorMessage(result.message)
+            setLoginError(true)
         }
 
-        
-       
-    
+
+
+
     }
     return (
         <div className="LoginContainer">
@@ -49,20 +56,41 @@ function Login() {
                     <p>Manage your business in one page</p>
                 </div>
             </div>
-            <div style = {{backgroundImage: `url(/images/graybg.png)`,backgroundRepeat: 'no-repeat',backgroundSize: 'cover',backgroundPosition: 'center'}} className="LoginContainersecondbox">
+            <div style={{ backgroundImage: `url(/images/graybg.png)`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }} className="LoginContainersecondbox">
                 <div className="LoginContainersecondboxform">
                     <h2>Login</h2>
+                   
                     <form className="formHouse">
 
                         <input className="forminput"
-                        onChange = {(e)=> setEmail(e.target.value)}
-                        type="text" name="username" placeholder="Username" />
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="text" name="username" placeholder="Username" />
                         <input className="forminput" type="password"
-                        onChange = {(e)=> setPassword(e.target.value)}
-                        name="password" placeholder="Password" />
+                            onChange={(e) => setPassword(e.target.value)}
+                            name="password" placeholder="Password" />
                         <span>Forgot password</span>
-                        <button className="submitbut" onClick ={login}>Login</button>
+                       <div style = {{
+                           display:'flex',
+                           alignItems: 'center',
+                           marginTop:20,
+
+                       }}>
+                       <button className="submitbut" onClick={login}>Login</button>
+                        <i class="fa fa-camera-retro fa-2x" style = {{
+                            marginLeft:10
+                        }}></i>
+                       </div>
+                        
+                        
                     </form>
+
+
+                    {loginError ? <>
+
+                        <div className="errorLogin"><h3>{errorMessage} </h3> </div>
+
+                    </> : null}
+
 
                 </div>
             </div>
