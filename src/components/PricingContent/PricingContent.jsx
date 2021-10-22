35ca65f929a0a3163d91096a1a2073247ad25bc2
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-10-22 23:51:52 +0100

    Trying to add drawer

--- src/components/PricingContent/PricingContent.jsx
+++ src/components/PricingContent/PricingContent.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-import {useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import GlobalStyles from "@mui/material/GlobalStyles";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import Grid from '@mui/material/Grid';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import Box from '@mui/material/Box';
-import StarIcon from '@mui/icons-material/StarBorder';
-import Link from '@mui/material/Link';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-
-
-
-
+import Grid from "@mui/material/Grid";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import StarIcon from "@mui/icons-material/Star";
+import Link from "@mui/material/Link";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import MenuIcon from "@mui/icons-material/Menu";
+// import List from "@mui/material/List";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+// import ListItemText from "@mui/material/ListItemText";
+// import InboxIcon from "@mui/icons-material/MoveToInbox";
+// import MailIcon from "@mui/icons-material/Mail";
+// import Drawer from "@mui/material/Drawer";
 const tiers = [
   {
     title: "Free",
@@ -60,48 +65,88 @@ const tiers = [
 ];
 
 const footers = [
-    {
-      title: 'Company',
-      description: ['Team', 'History', 'Contact us', 'Locations'],
-    },
-    {
-      title: 'Features',
-      description: [
-        'Cool stuff',
-        'Random feature',
-        'Team feature',
-        'Developer stuff',
-        'Another one',
-      ],
-    },
-    {
-      title: 'Resources',
-      description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
-    },
-    {
-      title: 'Legal',
-      description: ['Privacy policy', 'Terms of use'],
-    },
-  ];
-  
+  {
+    title: "Company",
+    description: ["Team", "History", "Contact us", "Locations"],
+  },
+  {
+    title: "Features",
+    description: [
+      "Cool stuff",
+      "Random feature",
+      "Team feature",
+      "Developer stuff",
+      "Another one",
+    ],
+  },
+  {
+    title: "Resources",
+    description: [
+      "Resource",
+      "Resource name",
+      "Another resource",
+      "Final resource",
+    ],
+  },
+  {
+    title: "Legal",
+    description: ["Privacy policy", "Terms of use"],
+  },
+];
 
-  function Copyright(props) {
-    return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
-        {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
-          Asterix
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
-
-
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Asterix
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 function PricingContent() {
-    const history = useHistory(); 
+  const history = useHistory();
+  const matches = useMediaQuery("(min-width:600px)");
+  const [state, setState] = React.useState(false);
+
+  const toggleDrawer = () => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState(!state);
+  };
+
+  // const list = () => (
+  //   <Box
+  //     sx={{ width: 250 }}
+  //     role="presentation"
+  //     onClick={toggleDrawer()}
+  //     onKeyDown={toggleDrawer()}
+  //   >
+  //     <List>
+  //       {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+  //         <ListItem button key={text}>
+  //           <ListItemIcon>
+  //             {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+  //           </ListItemIcon>
+  //           <ListItemText primary={text} />
+  //         </ListItem>
+  //       ))}
+  //     </List>
+  //   </Box>
+  // );
   return (
     <React.Fragment>
       <GlobalStyles
@@ -109,54 +154,102 @@ function PricingContent() {
       />
       <CssBaseline />
 
-
       <AppBar
         position="sticky"
         color="default"
         elevation={4}
-        sx={{backgroundColor:`#18A0FB`, borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+        sx={{
+          backgroundColor: `#18A0FB`,
+          borderBottom: (theme) => `1px solid ${theme.palette.divider}`,
+        }}
       >
-        <Toolbar sx={{ flexWrap: 'wrap' }}>
-          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1, cursor: `pointer`, color:`white` }} onClick = {()=>{
-              history.push('/');
-          }}>
+        <Toolbar sx={{ flexWrap: "wrap" }}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1, cursor: `pointer`, color: `white` }}
+            onClick={() => {
+              history.push("/");
+            }}
+          >
             Asterix
           </Typography>
-          <nav>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5, textDecoration:`none`, color:`white` }}
+
+          {matches ? (
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
             >
-              Features
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5, textDecoration:`none`, color:`white`}}
-            >
-              Enterprise
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5, textDecoration:`none`, color:`white` }}
-            >
-              Support
-            </Link>
-          </nav>
-          <Button variant="outlined" sx={{ my: 1, mx: 1.5,color:`white` }} 
-          onClick = {()=>{
-              history.push('/login');
-          }}>
-            Login
-          </Button>
+              <nav>
+                <Link
+                  variant="button"
+                  color="text.primary"
+                  href="#"
+                  sx={{
+                    my: 1,
+                    mx: 1.5,
+                    textDecoration: `none`,
+                    color: `white`,
+                  }}
+                >
+                  Features
+                </Link>
+                <Link
+                  variant="button"
+                  color="text.primary"
+                  href="#"
+                  sx={{
+                    my: 1,
+                    mx: 1.5,
+                    textDecoration: `none`,
+                    color: `white`,
+                  }}
+                >
+                  Enterprise
+                </Link>
+                <Link
+                  variant="button"
+                  color="text.primary"
+                  href="#"
+                  sx={{
+                    my: 1,
+                    mx: 1.5,
+                    textDecoration: `none`,
+                    color: `white`,
+                  }}
+                >
+                  Support
+                </Link>
+              </nav>
+              <Button
+                variant="outlined"
+                sx={{
+                  my: 1,
+                  mx: 1.5,
+                  color: `white`,
+                  backgroundColor: `orange`,
+                }}
+                onClick={() => {
+                  history.push("/login");
+                }}
+              >
+                Login
+              </Button>
+            </div>
+          ) : (
+            <MenuIcon
+              sx={{
+                color: `white`,
+                cursor: `pointer`,
+              }}
+              onClick={() => toggleDrawer()}
+            />
+          )}
         </Toolbar>
       </AppBar>
-
 
       <Container
         disableGutters
@@ -200,7 +293,11 @@ function PricingContent() {
                   title={tier.title}
                   subheader={tier.subheader}
                   titleTypographyProps={{ align: "center" }}
-                  action={tier.title === "Pro" ? <StarIcon /> : null}
+                  action={
+                    tier.title === "Pro" ? (
+                      <StarIcon sx={{ color: `green` }} />
+                    ) : null
+                  }
                   subheaderTypographyProps={{
                     align: "center",
                   }}
@@ -255,7 +352,6 @@ function PricingContent() {
         </Grid>
       </Container>
 
-
       <Container
         maxWidth="md"
         component="footer"
@@ -284,11 +380,14 @@ function PricingContent() {
           ))}
         </Grid>
         <Copyright sx={{ mt: 5 }} />
+        <span>{`(min-width:600px) matches: ${matches}`}</span>
+{/* 
+        <Drawer anchor={state} open={state} onClose={() => setState(false)}>
+          {list()}
+        </Drawer> */}
       </Container>
     </React.Fragment>
   );
 }
-
-
 
 export default PricingContent;
