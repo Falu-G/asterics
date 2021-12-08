b3ae5652fcefc2e0f9ceb216411ffdbcd13eb69e
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-12-08 02:04:56 +0100

    make it fly

--- src/components/TemporaryDrawer/TemporaryDrawer.jsx
+++ src/components/TemporaryDrawer/TemporaryDrawer.jsx
@@ -1,15 +1,16 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import QuizIcon from "@mui/icons-material/Quiz";
+import GroupsIcon from "@mui/icons-material/Groups";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
+import LoginIcon from '@mui/icons-material/Login';
 export default function TemporaryDrawer() {
   const [state, setState] = React.useState({
     top: false,
@@ -19,25 +20,43 @@ export default function TemporaryDrawer() {
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
 
     setState({ ...state, [anchor]: open });
   };
 
+  const DisplayIcon = ({ icon }) => {
+    switch (icon) {
+      case "FAQ":
+        return <QuizIcon />;
+      case "Contact Us":
+        return <ContactMailIcon />;
+      case "Team":
+        return <GroupsIcon />;
+        case "Login":
+            return <LoginIcon />;
+      default:
+        return null;
+    }
+  };
+
   const list = (anchor) => (
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        {['FAQ', 'Contact Us', 'Team', 'Login'].map((text, index) => (
+        {["FAQ", "Contact Us", "Team", "Login"].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              <DisplayIcon icon={text} href="../../pages/Login/Login" />
             </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
@@ -49,9 +68,9 @@ export default function TemporaryDrawer() {
 
   return (
     <div>
-      {['left'].map((anchor) => (
+      {["left"].map((anchor) => (
         <React.Fragment key={anchor}>
-          <MenuIcon onClick={toggleDrawer(anchor, true)}/>
+          <MenuIcon onClick={toggleDrawer(anchor, true)} />
           <Drawer
             anchor={anchor}
             open={state[anchor]}
