d65559c9e70ef401d283a97424634df14272bd95
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-12-21 00:30:42 +0100

    lasignup

--- src/pages/LaSignUp/LaSignUp.jsx
+++ src/pages/LaSignUp/LaSignUp.jsx
@@ -1,48 +1,37 @@
-import React from 'react'
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from "react";
+
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 
-function Copyright(props) {
-    return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
-        {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
-          Your Website
-        </Link>{' '}
-        {new Date().getFullYear()}
-      </Typography>
-    );
-  }
-
-  const theme = createTheme();
+const theme = createTheme();
 function LaSignUp() {
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        // eslint-disable-next-line no-console
-        console.log({
-          email: data.get('email'),
-          password: data.get('password'),
-        });
-      };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
 
-
-    return (
-         <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        component="main"
+        sx={matches? { height: "100vh", overflow: "hidden" }:{ height: "100vh"}}
+      >
         <CssBaseline />
         <Grid
           item
@@ -53,108 +42,170 @@ function LaSignUp() {
             backgroundColor: "#18a0fb",
           }}
         >
+          <Typography
+            variant="h4"
+            gutterBottom
+            component="div"
+            sx={{
+              color: "white",
+            }}
+          >
+            AsteriX
+          </Typography>
 
-<Typography variant="h4" gutterBottom component="div" sx = {{
-    color:"white"
-}}>
-        AsteriX
-      </Typography>
+          <Box
+            sx={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Typography
+              variant="h2"
+              gutterBottom
+              component="div"
+              sx={{
+                color: "white",
+                fontWeight: 700,
+              }}
+            >
+              Sign Up
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              component="div"
+              sx={{
+                color: "white",
+              }}
+            >
+              Create your new Account
+            </Typography>
 
-      <Box
-      sx={{
-        width: "100%",
-        height: "100%",
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"center",
-        flexDirection:"column",
-      }}
-    >
-<Typography variant="h2" gutterBottom component="div" sx = {{
-    color:"white",
-    fontWeight:700
-}}>
-        Sign Up
-      </Typography>
-      <Typography variant="h5" gutterBottom component="div" sx = {{
-    color:"white"
-}}>
-       Create your new Account
-      </Typography>
-
-      <Typography variant="body1" gutterBottom component="div" sx = {{
-    color:"white"
-}}>
-        and manage your account with a single sign on
-      </Typography>
-
-    </Box>
+            <Typography
+              variant="body1"
+              gutterBottom
+              component="div"
+              sx={{
+                color: "white",
+              }}
+            >
+              and manage your account with a single sign on
+            </Typography>
+          </Box>
         </Grid>
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
+              display: "flex",
+              flexDirection: "column",
             }}
           >
-         
             <Typography component="h1" variant="h5">
               Create an Account
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    id="standard-search"
+                    label="Company name"
+                    type="search"
+                    fullWidth
+                    variant="standard"
+                  />
                 </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
+                <Grid item xs={6}>
+                  <TextField
+                    id="standard-search"
+                    label="Enter your first name"
+                    type="search"
+                    fullWidth
+                    variant="standard"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    id="standard-search"
+                    label="Enter your last name"
+                    type="search"
+                    fullWidth
+                    variant="standard"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="standard-search"
+                    label="Enter your email address"
+                    type="search"
+                    fullWidth
+                    variant="standard"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="standard-search"
+                    label="Enter your phone number"
+                    type="search"
+                    fullWidth
+                    variant="standard"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="standard-search"
+                    label="Username"
+                    type="search"
+                    fullWidth
+                    variant="standard"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    id="standard-search"
+                    label="Enter your password"
+                    type="search"
+                    fullWidth
+                    variant="standard"
+                  />
+                </Grid>
+
+                <Grid item xs={6}>
+                  <TextField
+                    id="standard-search"
+                    label="Confirm your password"
+                    type="search"
+                    fullWidth
+                    variant="standard"
+                  />
+                </Grid>
+
+                <Grid item xs={12}
+                sx={{ flexGrow: 1, alignItems: 'center',justifyContent: 'center' ,display: 'flex'}}>
+              
+
+                <Button variant="contained">Sign Up</Button>
+               
+               
                 </Grid>
               </Grid>
-              <Copyright sx={{ mt: 5 }} />
             </Box>
           </Box>
         </Grid>
       </Grid>
     </ThemeProvider>
-    )
+  );
 }
 
-export default LaSignUp
+
+export default LaSignUp;
