dee4b858365d0141b13de934d42e0dabb093e9a8
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2022-01-08 22:59:29 +0100

    push

--- src/pages/LaSignUp/LaSignUp.jsx
+++ src/pages/LaSignUp/LaSignUp.jsx
@@ -63,7 +63,7 @@ function LaSignUp() {
     setRegistering(true);
 
     try {
-      let result = await fetch("https://asteric.herokuapp.com/users/register", {
+      let result = await fetch("http://20.107.4.58:8081/users/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
